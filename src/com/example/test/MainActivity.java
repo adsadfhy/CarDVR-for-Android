22f77f52baa8511ecffdf990757f3d20b14c34b8
adsadfhy <adsadfhy@163.com>
2014-11-24 14:41:20 +0800

    Added OK button

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +14,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final TextView textView = (TextView) findViewById(R.id.textView1);
+		Button okButton = (Button) findViewById(R.id.button1);
+		okButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				textView.setText("OK");
+			}
+		});
 	}
 
 	@Override
